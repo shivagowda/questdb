6cd443376496a1b4c45de01da5e8f78396ededab
Alexander Pelagenko <2159629+ideoma@users.noreply.github.com>
2021-06-04 14:01:22 +0100

    fix C mac os build

--- core/src/main/c/linux/files.c
+++ core/src/main/c/linux/files.c
@@ -34,7 +34,7 @@
 #include <sys/fcntl.h>
 
 
-inline jlong _io_questdb_std_Files_mremap0
+static inline jlong _io_questdb_std_Files_mremap0
         (jlong fd, jlong address, jlong previousLen, jlong newLen, jlong offset, jint flags) {
     int prot = 0;
 
